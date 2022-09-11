e5a414752006c07019078fdd6f294107a77aa7f2
v3lyv3l4 <112582718+v3lyv3l4@users.noreply.github.com>
2022-09-11 13:04:33 -0700

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Happy)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Sad)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+})
